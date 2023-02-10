52ecedc307c20ff04f7ca20d31b0685c31d6817c
Tobias1R <tobias.rocha@craftablesoftware.com>
2023-02-10 13:57:42 -0300

    process pipeline definition

--- borderliner/core/process.py
+++ borderliner/core/process.py
@@ -2,3 +2,8 @@ from .pipelines import (
     Pipeline, PipelineConfig
 )
 
+class ProcessPipeline(Pipeline):
+    def __init__(self, config: PipelineConfig | str, *args, **kwargs) -> None:
+        super().__init__(config, *args, **kwargs)
+    
+    
